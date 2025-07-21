376c6233190fd24da7116b3a6b84528af0085ef6
Alexandro Arauco Sanchez <xander.as90@gmail.com>
2025-07-21 10:27:43 +0200

    fix: Update API authorization header to use environment variable

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,8 +5,7 @@ const axiosInstance = axios.create({
   timeout: 10000,
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOGZmODE2MTQ1ZmJlNWM3YTQzMTFmYjhkZjg0MzQyYiIsIm5iZiI6MTQ3NTY3OTY4OC42NzEsInN1YiI6IjU3ZjUxNWM4OTI1MTQxMWM4MTAwNTdlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XeefOehmE55bOqX8tTyFPWm54T7BkEbyba06qoT_yaE",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_BEARER}`,
   },
 });
 
